2a0943cf31e4db14b8f8c8aac1b44b6ba825b9a7
miklos123 <horvath.miklos.357@dszcbaross.edu.hu>
2025-02-13 11:01:21 +0100

    fetch

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 window.addEventListener('DOMContentLoaded', fetchCharackters);
 
 async function fetchCharackters() {
+    try {
+        const response= await fetch('https://raw.githubusercontent.com/Laboratoria/LIM011-data-lovers/master/src/data/potter/potter.json');
+        console.log(response);
+    } catch (error) {
+        
+    }
+
     
 }
 
